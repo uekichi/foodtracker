09f3b773cb4b97b0400dac0f8535d6719c1dd92f
uekichi <uekichi@qc.commufa.jp>
2018-09-05 14:10:42 +0000

    ０７基本的なUIを作ろう、まで

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -6,10 +6,39 @@
 //  Copyright © 2018年 ウエキチ. All rights reserved.
 //
 
+
 import XCTest
 @testable import FoodTracker
 
+
 class FoodTrackerTests: XCTestCase {
+    
+    // MARK: Meal Class Tests
+    
+    // Confirm that the Meal initializer return a Meal object when passed valid parameters.
+    func testMealInitializationSucceeds() {
+        
+        // Zero rating
+        let zeroRatingMeal = Meal(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        
+        // Highest positive rating
+        let positiveRatingMeal = Meal(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+        
+        // Negative rating
+        let negativeRatingMeal = Meal(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        
+        // Empty String
+        let emptyStringMeal = Meal(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+        
+        // Rating exceeds maximum
+        let largeRatingMeal = Meal(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+        
+    }
     
     override func setUp() {
         super.setUp()
