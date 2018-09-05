0e5bd56956bc66b4367ae38a9b1be606f37b471f
uekichi <uekichi@qc.commufa.jp>
2018-09-05 01:45:17 +0000

    ０６基本的なUIを作ろう２完了

--- FoodTracker/RatingControlViewController.swift
+++ FoodTracker/RatingControlViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class RatingControl: UIStackView {
+    // MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
     // MARK: Initialization
         override init(frame: CGRect) {
             super.init(frame: frame)
@@ -28,20 +33,25 @@ class RatingControl: UIStackView {
     // MARK: Private Methods
     
     private func setupButtons() {
-        // Create the button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        
-        // Add constraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        // Setup the button action
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
-        
-        // Add the button to the stack
-        addArrangedSubview(button)
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to rating button array
+            ratingButtons.append(button)
+        }
     }
     
 }
