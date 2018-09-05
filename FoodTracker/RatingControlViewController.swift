6d552430b8cf787167aedbcf28f9f322fe16eb37
uekichi <uekichi@qc.commufa.jp>
2018-09-05 00:02:52 +0000

    レーティングボタンの赤ボタンの確認まで

---
+++ FoodTracker/RatingControlViewController.swift
@@ -0,0 +1,47 @@
+//
+//  RatingControlViewController.swift
+//  FoodTracker
+//
+//  Created by ウエキチ on 2018/09/05.
+//  Copyright © 2018年 ウエキチ. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    // MARK: Initialization
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setupButtons()
+        }
+        
+        required init(coder: NSCoder) {
+            super.init(coder: coder)
+            setupButtons()
+        }
+    
+    // MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
+    // MARK: Private Methods
+    
+    private func setupButtons() {
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+    
+}
