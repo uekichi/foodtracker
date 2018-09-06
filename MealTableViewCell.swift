b867fdf966d896e58b2e30249eba08b260bd300c
uekichi <uekichi@qc.commufa.jp>
2018-09-06 07:59:25 +0000

    一覧セルのUIデザインまで

---
+++ MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by ウエキチ on 2018/09/06.
+//  Copyright © 2018年 ウエキチ. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
