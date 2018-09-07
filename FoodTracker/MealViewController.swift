26d0bcbc5890dfb80dfa17fec474f85e5795c1c7
uekichi <uekichi@qc.commufa.jp>
2018-09-07 07:52:16 +0000

    09の終わりまで

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -39,6 +39,10 @@ class MealViewController: UIViewController {
     }
 
     // MARK: Navigation
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     // This method lets you configure a view controller before it's presented.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
