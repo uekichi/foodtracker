1a21567a5671a6ab8c771944a0d6ad8db213415a
uekichi <uekichi@qc.commufa.jp>
2018-09-08 03:51:58 +0000

    cancelボタン操作を修正

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -48,7 +48,16 @@ class MealViewController: UIViewController {
 
     // MARK: Navigation
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingIndAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingIndAddMealMode {
+            dismiss(animated: true, completion: nil)
+        } else if let owingNavigationController = navigationController {
+            owingNavigationController.popViewController(animated: true)
+        } else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
     
     // This method lets you configure a view controller before it's presented.
